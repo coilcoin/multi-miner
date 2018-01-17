47ed9971e734862c9cc75928285587e7ed528b16
ChurchillSD <ChurchillSD@users.noreply.github.com>
2018-01-17 17:20:19 +0000

    added user choice of threads
    User can now choose how much they want there computer to slow down by.....

--- mutil-miner.py
+++ mutil-miner.py
@@ -1,31 +1,36 @@
 import threading
 from mining import mine
+import math
+
+def primeNumber(count):
+    while True:
+        isprime = True
+        
+        for x in range(2, int(math.sqrt(count) + 1)):
+            if count % x == 0: 
+                isprime = False
+                break
+            
+        if isprime:
+            return(str(count))
+            
+        count += 1
+    
+
+
+
+
 
 def main():
     
     print("welcome to hacky miner2")
     print("we be mining all t'day")
-    
-    t1 = threading.Thread(target=mine, args=("1",))
-    t2 = threading.Thread(target=mine, args=("27",))
-    t3 = threading.Thread(target=mine, args=("97",))
-    t4 = threading.Thread(target=mine, args=("193",))
-    t5 = threading.Thread(target=mine, args=("941",))
-    t6 = threading.Thread(target=mine, args=("571",))
-    t7 = threading.Thread(target=mine, args=("409",))
-    t8 = threading.Thread(target=mine, args=("719",))
 
-    
-    t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
-    t5.start()
-    t6.start()
-    t7.start()
-    t8.start()
+    threads = int(input("How many threads would you like to use? "))
+
+    for i in range(1, threads):
+        threading.Thread(target=mine, args=(primeNumber(i*10^i),)).start()
         
+
+
 main()
-    
-
-
